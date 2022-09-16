8e28fca2a8fb24d43f276dcc7c80e4b5edf245ff
SangeethaPulgam <113576649+SangeethaPulgam@users.noreply.github.com>
2022-09-16 09:46:29 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import joblib
+model_nb=joblib.load('winequalitydetection')
+st.title('WINE QUALITY DETECTION')
+ip=st.text_input("enter the type:")
+op=model_nb.predict([ip])
+if st.button('predict'):
+  st.title(op[0])
+  
