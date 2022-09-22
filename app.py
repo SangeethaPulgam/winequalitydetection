21f5f0d5260fd971f5c9ace419232dce6556030f
SangeethaPulgam <113576649+SangeethaPulgam@users.noreply.github.com>
2022-09-22 07:47:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model_nb=joblib.load('wine-quality(1)')
+model_nb=joblib.load('wine-quality')
 st.title('WINE QUALITY DETECTION')
 ip=st.text_input("enter the type:")
 op=model_nb.predict([ip])
