88dcd74ae29c5f83c003cc11ed4123d475567702
SangeethaPulgam <113576649+SangeethaPulgam@users.noreply.github.com>
2022-09-23 09:51:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 model_nb=joblib.load('wine-quality')
 
 st.title('WINE QUALITY DETECTION')
-ip=st.text_input('enter the type :')
+ip=st.text_input('enter the text :')
 op=model_nb.predict([ip])
 if st.button('predict'):
      st.title(op[0])
